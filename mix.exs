8dbeb6c938ba95e69847d33a57689e4851687b98
JohnnyT <ubergeek3141@gmail.com>
2020-06-17 20:23:44 -0600

    Prepares UXID v0.0.1 (EX)
    This prepares to publish v0.0.1 of the Hex package.

--- mix.exs
+++ mix.exs
@@ -1,28 +1,62 @@
 defmodule UXID.MixProject do
   use Mix.Project
 
-  def project do
+  @name "UXID"
+  @app :uxid
+  @description "User eXperience focused IDentifiers"
+  @version "0.0.1"
+
+  @deps [
+    # Required
+
+    # Development, Documentation, Testing, ...
+    {:ex_doc, "~> 0.21", only: :dev}
+  ]
+
+  def application(), do: []
+
+  def project() do
     [
-      app: :uxid,
-      version: "0.0.1",
+      aliases: aliases(),
+      app: @app,
+      name: @name,
+      description: @description,
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: @deps,
+      elixirc_options: [warnings_as_errors: true],
+      elixirc_paths: elixirc_paths(Mix.env()),
+      preferred_cli_env: preferred_cli_env(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  # ---------------------------------------------------------------------------
+  # Private
+
+  defp aliases() do
     [
-      extra_applications: [:logger]
+      coverage: ["coveralls.html"]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp elixirc_paths(:test), do: ["test/support", "lib"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/riddler/uxid"}
+    ]
+  end
+
+  defp preferred_cli_env() do
+    [
+      coverage: :test,
+      coveralls: :test,
+      "coveralls.html": :test
     ]
   end
 end
